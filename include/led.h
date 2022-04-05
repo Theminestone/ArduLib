ee69d1b9b0baf672cc5a619249f878c014d4d5f3
Theminestone <gabriel.unterweger05@gmail.com>
2022-04-05 19:25:30 +0200

    Commit

--- include/led.h
+++ include/led.h
@@ -1,9 +1,24 @@
 #ifndef ARDULIB_LED_H
 #define ARDULIB_LED_H
 
+#include <Arduino.h>
 
-class led {
 
+class LED {
+private:
+    byte _pin;
+    bool _status;
+    unsigned long _interval;
+    unsigned long _lastToggle;
+
+public:
+    LED(int pin, unsigned long interval);
+
+    void on();
+
+    void off();
+
+    void toggle();
 };
 
 
--- src/led.cpp
+++ src/led.cpp
@@ -1,1 +1,19 @@
 #include "led.h"
+
+LED::LED(int pin, unsigned long interval) {
+    _pin = pin;
+    _interval = interval;
+    pinMode(_pin, OUTPUT);
+}
+
+void LED::on() { digitalWrite(_pin, HIGH); }
+
+void LED::off() { digitalWrite(_pin, LOW); }
+
+void LED::toggle() {
+    if (_interval < millis() - _lastToggle) {
+        _status = !_status;
+        digitalWrite(_pin, _status);
+        _lastToggle = millis();
+    }
+}
